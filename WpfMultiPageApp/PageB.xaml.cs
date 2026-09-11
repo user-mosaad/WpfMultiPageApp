6c611db7c57ed1b2edbe1793c1c5f3a38868f2cc
user-mosaad <msaadshahid999@gmail.com>
2026-09-11 21:41:57 +0600

    Add Page A and Page B

---
+++ WpfMultiPageApp/PageB.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMultiPageApp
+{
+    /// <summary>
+    /// Interaction logic for PageB.xaml
+    /// </summary>
+    public partial class PageB : UserControl
+    {
+        public PageB()
+        {
+            InitializeComponent();
+        }
+    }
+}
